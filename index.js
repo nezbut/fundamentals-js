593e32e88d6a065c917ae4d9de19b0a206d24058
nezbut <anonim@anonim.com>
2024-12-05 11:29:57 +0700

    feat(index.js): complete task

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function concatStrings(a, b) {
+  return a + b;
+}
 
+function isString(value) {
+  return typeof value === "string";
+}
+
+function identifySign(value) {
+  if (value === 0) {
+    return "Ноль";
+  } else if (value > 0) {
+    return "Положительное число";
+  } else {
+    return "Отрицательное число";
+  }
+}
+
+function reverseWords(text) {
+  return text.split(" ").reverse().join(" ");
+}
+
+function wordsCount(text) {
+  return text.split(" ").length;
+}
+
+export { concatStrings, isString, identifySign, reverseWords, wordsCount };
